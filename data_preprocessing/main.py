a1cf9e0d77b401635121323925ac45ff8e56cc21
scupdev <ot.gaming132@gmail.com>
2023-03-26 14:04:19 +0530

    coded the dependant variable in the data preprocessing area

--- data_preprocessing/main.py
+++ data_preprocessing/main.py
@@ -2,11 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dataset = pd.read_csv('Data.csv')
-print(dataset)
+dataset = pd.read_csv('data.csv')
 
-matrix = dataset.iloc[:, :-1] # matrix from the dataset (Country, Age, Salary)
+matrix = dataset.iloc[:, :-1].values # matrix from the dataset (Country, Age, Salary)
 # iloc takes the no. of rows and columns from the data frame.
 # dataset.iloc[:, :-1] the first element specifies the row and the last one in the dict represents the columns
-dependant_variable = dataset.
-
+dependant_variable = dataset.iloc[:, -1].values
+# dependant variable is usually the last column of our dataset
+# -1 is the last value of the number of columns (Purchased in the data)
